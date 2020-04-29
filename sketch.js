4ba235eed805ebb0cbb207a8b7d868d2da895230
Hyejin Im <hyejinim17@gmail.com>
2020-04-29 10:47:38 -0400

    Fix errors

--- sketch.js
+++ sketch.js
@@ -5,29 +5,71 @@ var w = 640,
 var raster, param, pmat, resultMat, detector;
 var detected, gridWidth, gridHeight;
 
-function setup() {
-    pixelDensity(1); // this makes the internal p5 canvas smaller
-    capture = createCapture({
-        audio: false,
-        video: {
-            width: w,
-            height: h
-        }
-    }, function() {
-        console.log('capture ready.')
-    });
-    capture.elt.setAttribute('playsinline', '');
-    createCanvas(w, h);
-    capture.size(w, h);
-    capture.hide();
+var tracker;
 
-    raster = new NyARRgbRaster_Canvas2D(canvas);
-    param = new FLARParam(canvas.width, canvas.height);
-    pmat = mat4.identity();
-    param.copyCameraMatrix(pmat, 100, 10000);
-    resultMat = new NyARTransMatResult();
-    detector = new FLARMultiIdMarkerDetector(param, 2);
-    detector.setContinueMode(true);
+var rhi, ghi, bhi;
+var rlo, glo, blo;
+
+function setTarget(r, g, b, range) {
+    range = range || 32;
+    rhi = r + range, rlo = r - range;
+    ghi = g + range, glo = g - range;
+    bhi = b + range, blo = b - range;
+}
+
+function setup() {
+  pixelDensity(1); // this makes the internal p5 canvas smaller
+  capture = createCapture({
+      audio: false,
+      video: {
+          width: w,
+          height: h
+      }
+  }, function() {
+      console.log('capture ready.')
+  });
+  capture.elt.setAttribute('playsinline', '');
+  createCanvas(w, h);
+  capture.size(w, h);
+  capture.parent('container');
+  cnv = createCanvas(w, h);
+  cnv.parent('container');
+  // capture.hide(); // tracking.js can't track the video when it's hidden
+
+  raster = new NyARRgbRaster_Canvas2D(canvas);
+  param = new FLARParam(canvas.width, canvas.height);
+  pmat = mat4.identity();
+  param.copyCameraMatrix(pmat, 100, 10000);
+  resultMat = new NyARTransMatResult();
+  detector = new FLARMultiIdMarkerDetector(param, 2);
+  detector.setContinueMode(true);
+
+  setTarget(0, 0, 0); // by default track white
+
+  tracking.ColorTracker.registerColor('match', function (r, g, b) {
+    if (r <= rhi && r >= rlo &&
+        g <= ghi && g >= glo &&
+        b <= bhi && b >= blo) {
+        return true;
+    }
+    return false;
+    console.log("line 56");
+  });
+  tracker = new tracking.ColorTracker(['match']);
+  tracker.minDimension = 20; // make this smaller to track smaller objects
+  capture.elt.id = 'p5video';
+  tracking.track('#p5video', tracker, {
+    camera: true
+  });
+  tracker.on('track', function (event) {
+    cnv.clear();
+    strokeWeight(4);
+    stroke(255, 0, 0);
+    noFill();
+    event.data.forEach(function (r) {
+        rect(r.x, r.y, r.width, r.height);
+    })
+  });
 }
 
 function draw() {
@@ -113,4 +155,12 @@ function draw() {
   } 
   select('#gridWidth').elt.innerText = gridWidth;
   select('#gridHeight').elt.innerText = gridHeight;
+
+  if (mouseIsPressed &&
+    mouseX > 0 && mouseX < width &&
+    mouseY > 0 && mouseY < height) {
+      capture.loadPixels();
+      target = capture.get(mouseX, mouseY);
+      setTarget(target[0], target[1], target[2]);
+    }
 }
