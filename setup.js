505cb66573154a345a7e1fd2b3aa98bdeb4fcf42
Hyejin Im <hyejinim17@gmail.com>
2020-07-06 13:02:59 -0400

    Add tutorial

--- setup.js
+++ setup.js
@@ -1,7 +1,15 @@
 // detect HTTP or HTTPS then force HTTPS
 if (location.protocol != 'https:') {
     location.href = 'https:' + window.location.href.substring(window.location.protocol.length);
-   }
+}
+
+if (window.matchMedia("(orientation: portrait)").matches) {
+    // you're in PORTRAIT mode
+ }
+ 
+ if (window.matchMedia("(orientation: landscape)").matches) {
+    // you're in LANDSCAPE mode
+}
 
 // the video
 let capture;
